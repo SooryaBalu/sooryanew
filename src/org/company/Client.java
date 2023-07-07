6ccfe1a01f2cbf5ba98923784afa58c09510d846
SooryaBalu <sooryab1493@gmail.com>
2023-07-06 21:03:09 -0400

    message

---
+++ src/org/company/Client.java
@@ -0,0 +1,7 @@
+package org.company;
+
+public class Client {
+public void clientName() {
+	System.out.println("Client Name :Ruby");
+}
+}
---
+++ src/org/company/Company.java
@@ -0,0 +1,16 @@
+package org.company;
+
+public class Company extends Client{
+private void companyName() {
+	System.out.println("Company Name:Citi");
+	
+}
+public static void main(String[] args) {
+	Company n=new Company();
+	n.companyName();
+    n.clientName();
+
+}
+
+
+}
